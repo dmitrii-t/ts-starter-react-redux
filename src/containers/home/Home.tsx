430d46be9410fe15e17b9282e6d0c0f456a7145f
dtcimbal <dtcimbal@gmail.com>
2019-09-16 16:12:06 +1000

    Minor refactoring

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,11 +1,15 @@
 import Grid from '@material-ui/core/Grid'
 import * as React from 'react'
-import {withStyles} from '@material-ui/core'
+import {Typography, withStyles} from '@material-ui/core'
+import {Link} from 'react-router-dom'
 
 const styles = require('./Home.css')
 
 export const Home: React.FunctionComponent = () => (
     <Grid item xs={12}>
+        <Typography variant='subheading'>
+            Go to <Link to={'/data'}>Data</Link>
+        </Typography>
     </Grid>
 )
 
